68160fb226dd935347333f4ff9ed2e10d1b26196
sdass <sdass@drf.com>
2017-01-28 16:40:21 -0500

    content returned outside exect

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,14 +1,24 @@
+var exec = require("child_process").exec;
+
 function start() {
 	console.log("FROM requestHandler: 'start' was called.");
+
 
 	function sleep(milliseconds){
 		var startTime = new Date().getTime();
 		while(new Date().getTime() < startTime + milliseconds);
 	}
 	console.log("10 sec start sleep");
-	sleep(10000);
+	//sleep(10000);
 	console.log("ends 10 sec sleep");
-	return "start was called 10 second before";
+	var content = "EMPTY  ALL";
+	exec("ls -lah", function(error, stdout, stderr) { //asnych exec
+			//console.log("STDout" + stdout);
+			content = stdout;
+			console.log("content" + content);
+			return content; //child process
+		});
+	return content; // parent process //"start was called 10 second before";
 }
 
 
